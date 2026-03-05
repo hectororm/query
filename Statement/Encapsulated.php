da4bb2ebd8b243cb6eae782bbf6681f610710546
Ronan Giron <ElGigi@users.noreply.github.com>
2026-03-05 16:49:41 +0100

    Add Encapsulated statement wrapper class

---
+++ Statement/Encapsulated.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * This file is part of Hector ORM.
+ *
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @copyright 2026 Ronan GIRON
+ * @author    Ronan GIRON <https://github.com/ElGigi>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code, to the root.
+ */
+
+declare(strict_types=1);
+
+namespace Hector\Query\Statement;
+
+use Hector\Connection\Bind\BindParamList;
+use Hector\Connection\Driver\DriverCapabilities;
+use Hector\Query\StatementInterface;
+
+class Encapsulated implements StatementInterface
+{
+    /**
+     * Encapsulated constructor.
+     *
+     * @param StatementInterface $statement
+     */
+    public function __construct(
+        private StatementInterface $statement,
+    ) {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getStatement(
+        BindParamList $bindParams,
+        ?DriverCapabilities $driverCapabilities = null,
+        bool $encapsulate = false,
+    ): ?string {
+        $str = $this->statement->getStatement($bindParams, $driverCapabilities);
+
+        if (null === $str || '' === $str) {
+            return null;
+        }
+
+        return '( ' . $str . ' )';
+    }
+}
